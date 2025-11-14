d51cce5b308cb0a2a84d738ded4cef894cd4a8cc
MariaTorrecilla1904 <torrecillam5@gmail.com>
2025-11-13 21:14:02 -0500

    routes y controladores

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
+import categoriaRoutes from "./routes/categorias.routes.js";
+
 
 /*asignamos a app toda funcionalidad para un server web*/
 const app = express(); 
 
 /*setear*/
 app.set("port",5000)
+
+
+/*routes*/
+app.use("/api/categorias",categoriaRoutes)
 
 /*hacemos disponible app para todxa la aplicacion*/
 
