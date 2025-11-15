c82872b75659c0e2d9f2683eed79daacde864805
MariaTorrecilla1904 <torrecillam5@gmail.com>
2025-11-15 12:51:44 -0500

    frontend

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import categoriaRoutes from "./routes/categorias.routes.js";
-
+import cors from "cors"
 
 /*asignamos a app toda funcionalidad para un server web*/
 const app = express(); 
@@ -8,7 +8,7 @@ const app = express();
 /*setear*/
 app.set("port",5000)
 
-
+app.use(cors())
 /*routes*/
 app.use("/api/categorias",categoriaRoutes)
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import app from "./app.js";
 
-const main = () =>{
- app.listen(app.get("port"));
- console.log(`The company's server webbb is running on port${app.get("port")}`);
-}
+const main = () => {
+  app.listen(app.get("port"), () => {
+    console.log(`The company's server web is running on port ${app.get("port")}`);
+  });
+};
 
-main(); 
+main();
